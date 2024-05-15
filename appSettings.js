89aa5a010ebab1a25004028f6b2351929813d22d
angelina-wf <angelina.wolf@edu.fh-joanneum.at>
2024-05-15 15:38:14 +0200

    first commit

---
+++ appSettings.js
@@ -0,0 +1,13 @@
+const settings = {
+    'clientId': 'd3f2e24e-ea74-43ee-b98e-9a1cf90f6a29',
+    'tenantId': 'f8412f71-a2d4-4166-a594-a79041c6e476',
+    'graphUserScopes': [
+      'user.read',
+      'mail.read',
+      'mail.send',
+      'calendars.readwrite',
+      'tasks.readwrite'
+    ]
+  };
+  
+  module.exports = settings;
